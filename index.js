2b4706f2615e9ae7c83a65d660597991f6a0ad8d
willnelosn <49733596+willnelosn@users.noreply.github.com>
2019-05-11 09:39:28 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const token = 'NTcyNDk5MTUzMTMzODk1Njk4.XNIeWQ.bGxAmV7GjVkhpNMe2BnX30a9_GA';
 bot.login(token);
 
 bot.on('ready', () => {
-    bot.user.setGame('+ Help | Jimmy Dan in Da House')
+    bot.user.setGame('DM Will For Help')
   })
 if(command === '+help') {
   message.channel.send('If Jimmy Dean is away right now, you can DM him or Will for help!');
