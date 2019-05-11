e15fa31be00923061bf2662626279f059d15523f
willnelosn <49733596+willnelosn@users.noreply.github.com>
2019-05-11 09:45:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ bot.login(token);
 bot.on('ready', () => {
     bot.user.setGame('DM Will For Help')
   })
+
 if(command === '+help') {
   message.channel.send('If Jimmy Dean is away right now, you can DM him or Will for help!');
 } else
