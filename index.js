b1308b5776590258be2d507c36f5a41efd0a865f
oRexr <53697343+oRexr@users.noreply.github.com>
2019-08-04 12:24:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ var bot = new Discord.Client();
 
 bot.on("ready", () => {
   console.log("You are connected to " + bot.guilds.size + " servers!");
-  bot.user.setGame("Hey There", "https://www.twitch.tv/wilplayzyt")
+  bot.user.setGame("Hey There", "https://www.twitch.tv/oRexrHD")
 });
 
 
 
-bot.login('NTc5MDU4NDA2Mjc5ODcyNTUw.XUBzPw.V0RcOt5MH9fjZ86Yip5Aru_bmvI'); 
+bot.login('MTYzNzQ0MjU1MDYyNDQxOTg0.XUa_Og.tI82kPE_PguDZMrIVvxP5BEUCqQ'); 
